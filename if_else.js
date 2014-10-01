3492da299e94e226339882cbad28d0538905a954
ryanshim24 <ryanshim24@yahoo.com>
2014-10-01 16:03:51 -0700

    My if else

---
+++ if_else.js
@@ -0,0 +1,9 @@
+var foods = ["piiza", "tacos", "sushi", "bread", "rice"];
+
+if (foods.length > 1) {
+  console.log("You have lots of food");
+} else if (foods.length === 1) {
+  console.log("You have one food");
+} else {
+  console.log("You have no food");
+}
